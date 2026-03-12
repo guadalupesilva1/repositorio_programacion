41a01fc93c55c901cfbdc88dcf8c113177ce5b43
ClaraBaietti <cbaietti@udesa.edu.ar>
2026-03-12 15:25:14 -0300

    actualizacion programa

--- programa_principal.py
+++ programa_principal.py
@@ -2,7 +2,7 @@ import funciones_habitos
  
 actividad = input("ingrese una actividad: ") 
 
-lista = funciones_habitos.registrar_habitos()
+lista = funciones_habitos.registrar_habitos(actividad)
 resultado = funciones_habitos.analizar_habitos(lista)
 print("Resumen de actividades:")
 print(resultado)
